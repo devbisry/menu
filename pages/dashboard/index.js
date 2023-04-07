8f45803e20b5c99d3fe00573050ce5a04858e7af
devbisry <127805608+devbisry@users.noreply.github.com>
2023-04-07 13:59:04 +0000

    add dashboard table item

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,18 +1,34 @@
+import DataTable from "@/components/DashboardItems/Table";
 import DashboardLayout from "@/components/Layout/DashbordLayout";
-import Sidebar from "@/components/Sidebar/Sidebar";
+import { Box } from "@mui/material";
+import { GridSearchIcon } from "@mui/x-data-grid";
 
 export default function Dashboard() {
   return (
     <>
-      <h1>Dashboard</h1>
+      <h1 align="center">Dashboard</h1>
+      <Box
+        sx={{
+          display: "flex",
+        }}
+      >
+        <GridSearchIcon />
+      </Box>
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "2fr 1fr",
+          gap: 2,
+          padding: 2,
+        }}
+      >
+        <DataTable width="400" />
+        <DataTable height={300} />
+      </Box>
     </>
   );
 }
 
 Dashboard.getLayout = function getLayout(page) {
-  return (
-    <DashboardLayout>
-      {page}
-    </DashboardLayout>
-  )
-}
+  return <DashboardLayout>{page}</DashboardLayout>;
+};
